4b4a4bfd1cb5a87ec1d30b836f8f657e733121eb
Emsthf <49848313+emsthf@users.noreply.github.com>
2023-02-24 09:23:51 +0900

    feat: server module gradle setting

---
+++ server/basic-test/src/main/java/com/sp/sc/BasicTestApplication.java
@@ -0,0 +1,15 @@
+package com.sp.sc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicTestApplication {
+
+    public static void main(String[] args) {
+
+        Person person = null;
+
+        SpringApplication.run(BasicTestApplication.class, args);
+    }
+}
